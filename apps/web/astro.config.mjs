12c4f1406e0ad77062bebfec612275f04289b1c3
Fernando Moreno <morenomoranfernando@gmail.com>
2026-03-13 16:47:49 -0600

    Install tailwind and vue

--- apps/web/astro.config.mjs
+++ apps/web/astro.config.mjs
@@ -3,11 +3,16 @@ import node from "@astrojs/node";
 import tailwindcss from "@tailwindcss/vite";
 import { defineConfig, envField } from "astro/config";
 
+import vue from "@astrojs/vue";
+
 // https://astro.build/config
 export default defineConfig({
   output: "server",
   adapter: node({ mode: "standalone" }),
+
   vite: {
     plugins: [tailwindcss()],
   },
+
+  integrations: [vue()],
 });
